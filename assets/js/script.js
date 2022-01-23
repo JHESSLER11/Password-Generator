ae2e9307be42ce287cdd021260039a36217aa977
JHessler <jhessler11@gmail.com>
2022-01-23 13:01:00 -0500

    updated for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,46 +18,48 @@ function writePassword() {
 }
 // Generate password function
 function generatePassword () {
+  var password = [];
+  var actualPassword = "";
+  
   var character = prompt("How many characters do you want?")
-  //var password = ""
   //var passLength = character.length;
   //check if password length is between 8 and 128
   while (character < 8 || character > 128) {
     character = prompt("Your password must be between 8 and 128 characters")
   }
   //confirm character types in password
-    var confirmLower = confirm("Do you want the password to contain lowercase letters?");
-
-    var confirmUpper = confirm("Do you want the password to contain uppercase letters?");
-
-    var confirmNum = confirm("Do you want the password to contain numbers?");
-
-    var confirmSpecial  = confirm("Do you want the password to contain special characters?")
-    //determine which characters are added to the password based on the confirm inputs
-    if (confirmLower) {
-      password = password + lowCharSet;
-    }
-
-    if (confirmUpper) {
-      password = password + upCharSet;
-    }
-
-    if (confirmNum) {
-      password = password + numSet
-    }
-
-    if (confirmSpecial)  {
-      password = password + specialSet;
-    }
-    var actualPassword = ""
-
+  var confirmLower = confirm("Do you want the password to contain lowercase letters?");
+  
+  var confirmUpper = confirm("Do you want the password to contain uppercase letters?");
+  
+  var confirmNum = confirm("Do you want the password to contain numbers?");
+  
+  var confirmSpecial  = confirm("Do you want the password to contain special characters?")
+  //determine which characters are added to the password based on the confirm inputs
+  if (confirmLower) {
+    password = password + lowCharSet
+  }
+  
+  if (confirmUpper) {
+    password = password + upCharSet
+  }
+  
+  if (confirmNum) {
+    password = password + numSet
+  }
+  
+  if (confirmSpecial)  {
+    password = password + specialSet
+  }
+  console.log(password)
+  var length = character.length
+  
   for (let index = 0; index < length; index++) {
-    var randomIndex = Math.floor(Math.random() * password.length);
-    actualPassword += password.charAt(randomIndex);
+    var randomIndex = Math.floor(Math.random() *  password.length);
+    actualPassword += password.charAt(randomIndex)
     
   }
-  return password;
-
+return actualPassword;
 
 }
 // Add event listener to generate button
